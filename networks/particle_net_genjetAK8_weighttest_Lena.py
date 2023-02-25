2afef6a8d5447f7afaa71440a3a438b272a2b3a6
Lena Wild <lena.wild@oeaw.ac.at>
2023-02-25 10:12:39 +0100

    updates

--- networks/particle_net_genjetAK8_weighttest_Lena.py
+++ networks/particle_net_genjetAK8_weighttest_Lena.py
@@ -4,10 +4,10 @@ from torch import Tensor
 import math
 
 def get_model(data_config, **kwargs):
-    conv_params =  [(1, (3,3,3))]  # Note to myself: first parameter in conv_params is (probably) the number of connected neighbours
-    fc_params = [(2, 0.0)]           # fully connected layer (hidden_size, drop_out) after GNN for array based input
-    fc_global_params = [(40, 0.0)]   # fully connected layer (hidden_size, drop_out) for global features only
-    fc_combined_params = [(5, 0.0)] # fully connected layer (hidden_size, drop_out) for combined input from fc and fc_global
+    conv_params =  [(3, (300,300,300))] # Note to myself: first parameter in conv_params is (probably) the number of connected neighbours
+    fc_params = [(15, 0.0), (2, 0.0)]           # fully connected layer (hidden_size, drop_out) after GNN for array based input
+    fc_global_params = [(80, 0.0),(12, 0.0)]   # fully connected layer (hidden_size, drop_out) for global features only
+    fc_combined_params = [(5,0.0)] # fully connected layer (hidden_size, drop_out) for combined input from fc and fc_global
     use_fusion = False
     
     eflow_features_dims    = len(data_config.input_dicts['eflow_features'])
@@ -16,12 +16,12 @@ def get_model(data_config, **kwargs):
     num_classes = 2 
     model = ParticleNetTagger(eflow_features_dims, global_features_dims, num_classes,
                               conv_params, fc_params, fc_global_params, fc_combined_params, 
-                              use_fusion=use_fusion,
-                              use_fts_bn=kwargs.get('use_fts_bn', False),
-                              use_counts=kwargs.get('use_counts', False),
-                              constituents_input_dropout=kwargs.get('constituents_input_dropout', None),
-                              global_input_dropout=kwargs.get('global_input_dropout', None), 
-                              for_inference=kwargs.get('for_inference', False) 
+                              use_fusion=use_fusion, # applies batchnorm1D (?)
+                              use_fts_bn=kwargs.get('use_fts_bn', False), # ?
+                              use_counts=kwargs.get('use_counts', False), # ?
+                              constituents_input_dropout=kwargs.get('constituents_input_dropout', None), # drop out before ParticleNet
+                              global_input_dropout=kwargs.get('global_input_dropout', None),  # drop out before first DNN layer
+                              for_inference=kwargs.get('for_inference', False) # applies softmax at last step
                               )
 
     model_info = {
@@ -50,7 +50,7 @@ class LossLikelihoodFree(torch.nn.L1Loss):
 
         loss = weight*( self.base_points[0]*(target_lin-input[:,0]) + self.base_points[0]**2*(target_quad-input[:,1]) )**2
         for theta_base in self.base_points[1:]: #two base-points: 1,2
-            loss += weight*( theta_base*(target_lin-input[:,0]) + theta_base**2*(target_quad-input[:,1]) )**2
+            loss += weight*( theta_base*(target_lin-input[:,0]) + theta_base**2*(target_quad-input[:,1]) )**2 #removed the 0.5 
 
 
         if self.reduction == 'none':
